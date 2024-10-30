1ba7422edc13ab053c8a53e4e473763c0933ad5a
Timo Drick <timo.drick@7p-group.com>
2024-10-30 12:57:30 +0100

    Added commented dependency for new inset injection method.

--- edge_to_edge_preview_lib/build.gradle.kts
+++ edge_to_edge_preview_lib/build.gradle.kts
@@ -74,6 +74,8 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics:${Versions.composeVersion}")
     implementation("androidx.compose.ui:ui-tooling-preview:${Versions.composeVersion}")
 
+    //implementation("androidx.compose.ui:ui-test:1.8.0-alpha04")
+
     debugImplementation("androidx.compose.ui:ui-tooling:${Versions.composeVersion}")
     debugImplementation("androidx.compose.ui:ui-test-manifest:${Versions.composeVersion}")
 
