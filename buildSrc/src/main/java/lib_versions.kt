44fe9335127f64c0d4064d53504a293f9b5d793b
Timo Drick <timo@drick.de>
2025-04-27 11:21:08 +0200

    Update to compose bom 2025.04.01 and some other libs

--- buildSrc/src/main/java/lib_versions.kt
+++ buildSrc/src/main/java/lib_versions.kt
@@ -1,5 +1,5 @@
 object Versions {
-    const val mavenLib = "0.6.0"
+    const val mavenLib = "0.6.2"
     const val mavenGroupId = "de.drick.compose"
 
     const val benManesPlugin = "0.51.0" // Check for lib updates
@@ -13,19 +13,19 @@ object Versions {
 
     const val compileSdk = 35
 
-    const val kotlinSerialization = "1.7.3"
+    const val kotlinSerialization = "1.8.1"
     const val kotlinCoroutines = "1.9.0"
 
-    const val coreKtx = "1.15.0"
+    const val coreKtx = "1.16.0"
     const val lifecycle = "2.8.7"
 
     const val composeLintChecks = "1.4.2"
-    const val activityCompose = "1.9.3"
-    const val composeBom = "2024.09.03"
-    const val composeAdaptive = "1.0.0"
+    const val activityCompose = "1.10.1"
+    const val composeBom = "2025.04.01"
+    const val composeAdaptive = "1.1.0"
     // Currently there are problems when using bom.
     // MavenCentral do not validate the lib in this case
-    const val composeVersion = "1.7.5"
+    const val composeVersion = "1.8.0"
 
     const val junit = "4.13.2"
     const val extJunit = "1.2.1"
@@ -33,6 +33,6 @@ object Versions {
     const val uiAutomator = "2.4.0-alpha01"
     const val testRunner = "1.6.2"
     const val composeScreenshot = "0.0.1-alpha07"
-    const val robolectric = "4.14"
+    const val robolectric = "4.14.1"
     const val paparazzi = "1.3.4"
 }
