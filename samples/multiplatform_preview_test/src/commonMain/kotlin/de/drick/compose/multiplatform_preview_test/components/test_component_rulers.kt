850fb0a574021e59de08458c7e290c034f8e842f
Timo Drick <timo@drick.de>
2026-05-07 10:05:57 +0200

    Added a preview for test component

--- samples/multiplatform_preview_test/src/commonMain/kotlin/de/drick/compose/multiplatform_preview_test/components/test_component_rulers.kt
+++ samples/multiplatform_preview_test/src/commonMain/kotlin/de/drick/compose/multiplatform_preview_test/components/test_component_rulers.kt
@@ -1,16 +1,24 @@
 package de.drick.compose.multiplatform_preview_test.components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Preview
 @Composable
 private fun TestComponentPreview() {
-
+    TestComponent(
+        title = "Test component",
+        modifier = Modifier.fillMaxSize(),
+        middleModifier = Modifier.padding(24.dp),
+        innerModifier = Modifier.padding(16.dp)
+    )
 }
 
 @Composable
